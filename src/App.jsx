6261754346d621bf95a8883506d0bb3030d3f118
FlightofIcarus <victor.icarus@gmail.com>
2025-08-31 23:53:19 -0300

    refactor: extract home components from entrypoint App to Home file, importing this in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { ChakraProvider, Divider } from '@chakra-ui/react';
-import './App.css';
+import { Divider } from '@chakra-ui/react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <ChakraProvider>
+    <>
       <Header />
       <Divider />
       <Main />
       <Divider />
       <Footer />
-    </ChakraProvider>
+    </>
   );
 }
 
